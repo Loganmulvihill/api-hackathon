729f9645649e9e3204d9c5a15631dcf5f00afe2c
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-03-24 20:16:47 -0700

    played with row functionality

--- main.js
+++ main.js
@@ -122,6 +122,11 @@ function showEvents(json) {
     $("body").append("<p>" + json._embedded.events[i].name + "</p>");
     $("body").append("<p>" + json._embedded.events[i].locale + "</p>");
     $("body").append("<p>" + json._embedded.events[i].images[0].url + "</p>");
+    // $("body").append("<p>" + json._embedded.events[i].priceRanges[0].min + "</p>");
+    // $("body").append("<p>" + json._embedded.events[i].priceRanges[0].max + "</p>");
+    $("body").append("<p>" + json._embedded.events[i].dates.start.localDate + "</p>");
+    $("body").append("<p>" + json._embedded.events[i].dates.start.localTime + "</p>");
+    $("body").append("<p>" + json._embedded.events[i]._embedded.venues[0].name + "</p>");
   }
 }
 
@@ -146,22 +151,38 @@ function showEvents(json) {
 
 
 
+// renderEventRow(json){
+
+//   var table = this.tableElement.querySelector('tbody');
+
+//   var tr = document.createElement('tr');
+//   var name = document.createElement('td');
+//   name.textContent = data.name;
+
+//   var course = document.createElement('td');
+//   course.textContent = data.course;
+
+//   var grade = document.createElement('td');
+//   grade.textContent = data.grade;
+
+//   var td = document.createElement('td');
+//   var deleteButton = document.createElement('button');
+//   deleteButton.textContent = "Delete";
+//   deleteButton.className += "btn btn-danger";
+
+//   deleteButton.addEventListener('click', function () {
+//     deleteGrade(data.id);
+//   });
 
 
-
-
-
-
-// deleteGrade(id) {
-//   console.log(id)
-
-//   $.ajax({
-//     method: "DELETE",
-//     url: "https://sgt.lfzprototypes.com/api/grades/" + id,
-//     // data: { "name": name, "course": course, "grade": grade },
-//     headers: { "X-Access-Token": "QoYrtKFL" },
-//     success: this.handleDeleteGradeSuccess,
-//     error: this.handleDeleteGradeError
-//   })
+//   td.appendChild(deleteButton);
+//   tr.appendChild(name);
+//   tr.appendChild(course);
+//   tr.appendChild(grade);
+//   tr.appendChild(td);
+//   table.appendChild(tr);
+//   return tr;
 
 // }
+
+//     }
