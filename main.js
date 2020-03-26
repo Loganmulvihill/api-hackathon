16112d7056bc592fd13b0516eecb5d369cf96341
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-03-26 13:44:11 -0700

    finished mvp

--- main.js
+++ main.js
@@ -1,188 +1,150 @@
-// class App {
-//   constructor(gradeTable, pageHeader, gradeForm) {
-//     this.gradeTable = gradeTable;
-//     this.pageHeader = pageHeader;
-//     this.gradeForm = gradeForm;
-//     this.handleGetGradesError = this.handleGetGradesError.bind(this);
-//     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
-//     this.createGrade = this.createGrade.bind(this);
-//     this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
-//     this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
-//     this.deleteGrade = this.deleteGrade.bind(this);
-//     this.handleDeleteGradeError = this.handleDeleteGradeError.bind(this);
-//     this.handleDeleteGradeSuccess = this.handleDeleteGradeSuccess.bind(this);
 
-//   }
-
-//   handleGetGradesError(error) {
-//     console.error(error);
-//   }
-
-//   handleGetGradesSuccess(grades) {
-//     this.gradeTable.updateGrades(grades);
-//     var total = 0;
-//     for (var i = 0; i < grades.length; i++) {
-//       total += grades[i].grade
-//     }
-//     var average = total / grades.length;
-//     this.pageHeader.updateAverage(average);
-//   }
-
-//   getGrades() {
-//     $.ajax({
-//       method: "GET",
-//       url: "https://sgt.lfzprototypes.com/api/grades",
-//       headers: { "X-Access-Token": "QoYrtKFL" },
-//       success: this.handleGetGradesSuccess,
-//       error: this.handleGetGradesError
-//     })
-//   }
-
-//   start() {
-//     this.getGrades();
-//     this.gradeForm.onSubmit(this.createGrade);
-//     this.gradeTable.onDeleteClick(this.deleteGrade);
-//   }
-
-
-//   createGrade(name, course, grade) {
-//     console.log(name, course, grade);
-
-//     $.ajax({
-//       method: "POST",
-//       url: "https://sgt.lfzprototypes.com/api/grades",
-//       data: { "name": name, "course": course, "grade": grade },
-//       headers: { "X-Access-Token": "QoYrtKFL" },
-//       success: this.handleCreateGradeSuccess,
-//       error: this.handleCreateGradeError
-//     })
-
-
-//   }
-
-//   handleCreateGradeError(error) {
-//     console.error(error);
-//   }
-
-//   handleCreateGradeSuccess() {
-//     this.getGrades();
-//   }
-
-//   deleteGrade(id) {
-//     console.log(id)
-
-//     $.ajax({
-//       method: "DELETE",
-//       url: "https://sgt.lfzprototypes.com/api/grades/" + id,
-//       // data: { "name": name, "course": course, "grade": grade },
-//       headers: { "X-Access-Token": "QoYrtKFL" },
-//       success: this.handleDeleteGradeSuccess,
-//       error: this.handleDeleteGradeError
-//     })
-
-//   }
-
-//   handleDeleteGradeError(error) {
-//     console.error(error);
-//   }
-
-//   handleDeleteGradeSuccess() {
-//     this.getGrades();
-//   }
-// }
-
-// https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=ULaPAoWQUZyaEgtZCF9E39G7bEf00flf
-
+var searchValue = null;
 
 $.ajax({
   type: "GET",
-  url: "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=ULaPAoWQUZyaEgtZCF9E39G7bEf00flf",
+  url: "https://app.ticketmaster.com/discovery/v2/events.json?dmaId=381&apikey=ULaPAoWQUZyaEgtZCF9E39G7bEf00flf",
   async: true,
   dataType: "json",
   success: function (json) {
-    console.log(json);
-    // var e = document.getElementById("events");
-    // e.innerHTML = json.page.totalElements + " events found.";
     showEvents(json);
-    // initMap(position, json);
   },
-  // success: function (json) {
-  //   console.log(json);
-  //   // Parse the response.
-  //   // Do other things.
-  // },
   error: function (xhr, status, err) {
-    // This time, we do not end up here!
   }
 });
 
 
-function showEvents(json) {
+function searchInput() {
+
+  searchValue = document.getElementById('searchBar').value;
+  return searchValue;
+}
+
+function returnSearch() {
+  searchValue = document.getElementById('searchBar').value;
+  $.ajax({
+    type: "GET",
+    url: "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + searchValue + "&dmaId=381&apikey=ULaPAoWQUZyaEgtZCF9E39G7bEf00flf",
+    async: true,
+    dataType: "json",
+    success: function (json) {
+      updateSearch(json);
+    },
+    error: function (xhr, status, err) {
+    }
+  });
+
+}
+
+
+function updateSearch(json) {
+
+  var cardDeck = document.querySelector('.card-columns');
+  cardDeck.innerHTML = "";
+
+  var latlng = { lat: 33.6846, lng: -117.8265 };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: latlng
+  });
+
   for (var i = 0; i < json.page.size; i++) {
-    $("body").append("<p>" + json._embedded.events[i].name + "</p>");
-    $("body").append("<p>" + json._embedded.events[i].locale + "</p>");
-    $("body").append("<p>" + json._embedded.events[i].images[0].url + "</p>");
-    // $("body").append("<p>" + json._embedded.events[i].priceRanges[0].min + "</p>");
-    // $("body").append("<p>" + json._embedded.events[i].priceRanges[0].max + "</p>");
-    $("body").append("<p>" + json._embedded.events[i].dates.start.localDate + "</p>");
-    $("body").append("<p>" + json._embedded.events[i].dates.start.localTime + "</p>");
-    $("body").append("<p>" + json._embedded.events[i]._embedded.venues[0].name + "</p>");
+
+    var cardDeck = document.querySelector('.card-columns');
+    var card = document.createElement('div');
+    card.classList.add("card")
+
+    var image = document.createElement('img');
+    image.classList.add("card-img-top");
+    image.src = json._embedded.events[i].images[0].url;
+
+    var cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    var cardHeader = document.createElement('h5');
+    cardHeader.classList.add('card-title');
+    cardHeader.textContent = json._embedded.events[i].name;
+    var dateAndTime = document.createElement('p');
+    dateAndTime.classList.add('card-text');
+    dateAndTime.textContent = json._embedded.events[i].dates.start.localDate + " " + json._embedded.events[i].dates.start.localTime;
+    var timeZone = document.createElement('p');
+    timeZone.classList.add('card-text');
+    timeZone.textContent = "TimeZone: " + json._embedded.events[i].dates.timezone;
+
+
+
+    card.append(image);
+    cardBody.append(cardHeader);
+    cardBody.append(dateAndTime);
+    cardBody.append(timeZone);
+    card.append(cardBody);
+    cardDeck.append(card);
+
+    var latitude = parseFloat(json._embedded.events[i]._embedded.venues[0].location.latitude);
+    var longitude = parseFloat(json._embedded.events[i]._embedded.venues[0].location.longitude);
+    var latlng2 = { lat: latitude, lng: longitude };
+
+    var marker = new google.maps.Marker({
+      position: latlng2,
+      title: "Hello World!"
+    });
+
+    marker.setMap(map);
+    map.setCenter(latlng2);
+
+
+
   }
 }
 
 
+function showEvents(json) {
 
-// $.ajax({
-//   type: "GET",
-//   url: "https://app.ticketmaster.com/discovery/v2/classifications.json?apikey=ULaPAoWQUZyaEgtZCF9E39G7bEf00flf",
-//   async: true,
-//   dataType: "json",
-//   success: function (json) {
-//     console.log(json);
-//     // Parse the response.
-//     // Do other things.
-//   },
-//   error: function (xhr, status, err) {
-//     // This time, we do not end up here!
-//   }
-// });
+  var latlng = { lat: 33.6846, lng: -117.8265 };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: latlng
+  });
 
+  console.log(json);
+  for (var i = 0; i < json.page.size; i++) {
+    var cardDeck = document.querySelector('.card-columns');
+    var card = document.createElement('div');
+    card.classList.add("card")
 
+    var image = document.createElement('img');
+    image.classList.add("card-img-top");
+    image.src = json._embedded.events[i].images[0].url;
 
+    var cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    var cardHeader = document.createElement('h5');
+    cardHeader.classList.add('card-title');
+    cardHeader.textContent = json._embedded.events[i].name;
+    var dateAndTime = document.createElement('p');
+    dateAndTime.classList.add('card-text');
+    dateAndTime.textContent = json._embedded.events[i].dates.start.localDate + " " + json._embedded.events[i].dates.start.localTime;
+    var timeZone = document.createElement('p');
+    timeZone.classList.add('card-text');
+    timeZone.textContent = "TimeZone: " + json._embedded.events[i].dates.timezone;
 
+    card.append(image);
+    cardBody.append(cardHeader);
+    cardBody.append(dateAndTime);
+    cardBody.append(timeZone);
+    card.append(cardBody);
+    cardDeck.append(card);
 
-// renderEventRow(json){
+    var latitude = parseFloat(json._embedded.events[i]._embedded.venues[0].location.latitude);
+    var longitude = parseFloat(json._embedded.events[i]._embedded.venues[0].location.longitude);
+    var latLng2 = { lat: latitude, lng: longitude };
 
-//   var table = this.tableElement.querySelector('tbody');
+    var marker = new google.maps.Marker({
+      position: latLng2,
+      title: "Hello World!"
+    });
+    marker.setMap(map);
+    map.setCenter(latLng2);
 
-//   var tr = document.createElement('tr');
-//   var name = document.createElement('td');
-//   name.textContent = data.name;
+  }
 
-//   var course = document.createElement('td');
-//   course.textContent = data.course;
-
-//   var grade = document.createElement('td');
-//   grade.textContent = data.grade;
-
-//   var td = document.createElement('td');
-//   var deleteButton = document.createElement('button');
-//   deleteButton.textContent = "Delete";
-//   deleteButton.className += "btn btn-danger";
-
-//   deleteButton.addEventListener('click', function () {
-//     deleteGrade(data.id);
-//   });
-
-
-//   td.appendChild(deleteButton);
-//   tr.appendChild(name);
-//   tr.appendChild(course);
-//   tr.appendChild(grade);
-//   tr.appendChild(td);
-//   table.appendChild(tr);
-//   return tr;
-
-// }
-
-//     }
+}
