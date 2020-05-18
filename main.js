5711ba9365a35a22a868261693011f0e566bc67f
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-05-18 15:52:11 -0700

    deleted commented code

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 var searchValue = null;
 
 $.ajax({
@@ -11,9 +10,7 @@ $.ajax({
   }
 });
 
-
 function searchInput() {
-
   searchValue = document.getElementById('searchBar').value;
   return searchValue;
 }
@@ -31,15 +28,11 @@ function returnSearch() {
     error: function (xhr, status, err) {
     }
   });
-
 }
 
-
 function updateSearch(json) {
-
   var cardDeck = document.querySelector('.card-columns');
   cardDeck.innerHTML = "";
-
   var latlng = { lat: 33.6846, lng: -117.8265 };
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
@@ -47,15 +40,12 @@ function updateSearch(json) {
   });
 
   for (var i = 0; i < json.page.size; i++) {
-
     var cardDeck = document.querySelector('.card-columns');
     var card = document.createElement('div');
     card.classList.add("card, col-4")
-
     var image = document.createElement('img');
     image.classList.add("card-img-top");
     image.src = json._embedded.events[i].images[0].url;
-
     var cardBody = document.createElement('div');
     cardBody.classList.add('card-body');
     var cardHeader = document.createElement('h5');
@@ -67,52 +57,38 @@ function updateSearch(json) {
     var timeZone = document.createElement('p');
     timeZone.classList.add('card-text');
     timeZone.textContent = "TimeZone: " + json._embedded.events[i].dates.timezone;
-
-
-
     card.append(image);
     cardBody.append(cardHeader);
     cardBody.append(dateAndTime);
     cardBody.append(timeZone);
     card.append(cardBody);
     cardDeck.append(card);
-
     var latitude = parseFloat(json._embedded.events[i]._embedded.venues[0].location.latitude);
     var longitude = parseFloat(json._embedded.events[i]._embedded.venues[0].location.longitude);
     var latlng2 = { lat: latitude, lng: longitude };
-
     var marker = new google.maps.Marker({
       position: latlng2,
       title: "Hello World!"
     });
-
     marker.setMap(map);
     map.setCenter(latlng2);
-
-
-
   }
 }
 
-
 function showEvents(json) {
-
   var latlng = { lat: 33.6846, lng: -117.8265 };
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
     center: latlng
   });
-
   console.log(json);
   for (var i = 0; i < json.page.size; i++) {
     var cardDeck = document.querySelector('.card-columns');
     var card = document.createElement('div');
     card.classList.add("card")
-
     var image = document.createElement('img');
     image.classList.add("card-img-top");
     image.src = json._embedded.events[i].images[0].url;
-
     var cardBody = document.createElement('div');
     cardBody.classList.add('card-body');
     var cardHeader = document.createElement('h5');
@@ -124,25 +100,21 @@ function showEvents(json) {
     var timeZone = document.createElement('p');
     timeZone.classList.add('card-text');
     timeZone.textContent = "TimeZone: " + json._embedded.events[i].dates.timezone;
-
     card.append(image);
     cardBody.append(cardHeader);
     cardBody.append(dateAndTime);
     cardBody.append(timeZone);
     card.append(cardBody);
     cardDeck.append(card);
-
     var latitude = parseFloat(json._embedded.events[i]._embedded.venues[0].location.latitude);
     var longitude = parseFloat(json._embedded.events[i]._embedded.venues[0].location.longitude);
     var latLng2 = { lat: latitude, lng: longitude };
-
     var marker = new google.maps.Marker({
       position: latLng2,
       title: "Hello World!"
     });
     marker.setMap(map);
     map.setCenter(latLng2);
-
   }
 
 }
