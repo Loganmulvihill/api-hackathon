69b036ada6bba86b4c3ef91e5845ae30e097af92
Logan Mulvihill <mulvihill.logan@gmail.com>
2020-06-17 18:18:49 -0500

    created carousel functionality

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function returnSearch() {
 }
 
 function updateSearch(json) {
-  var cardDeck = document.querySelector('.scrolling-wrapper');
+  var cardDeck = document.querySelector('.carousel-inner');
   cardDeck.innerHTML = "";
   var latlng = { lat: 33.6846, lng: -117.8265 };
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -47,7 +47,10 @@ function updateSearch(json) {
   }
 
   for (var i = 0; i < json._embedded.events.length; i++) {
-    var cardDeck = document.querySelector('.scrolling-wrapper');
+    var cardDeck = document.querySelector('.carousel-inner');
+    var carouselItem = document.createElement('div');
+    carouselItem.classList.add("carousel-item");
+    carouselItem.classList.add("carousel-card");
     var card = document.createElement('div');
     card.classList.add("card")
     var image = document.createElement('img');
@@ -68,11 +71,17 @@ function updateSearch(json) {
     var monthAndDate = json._embedded.events[i].dates.start.localDate.slice(5);
     var formattedYear = monthAndDate + '-' + year;
     dateAndTime.textContent = formattedYear + " " + (getFormattedTime(timeHour, eventTime2));
+    var eventInfo = document.createElement('a');
+    eventInfo.classList.add('card-text');
+    eventInfo.textContent = 'Event Info';
+    eventInfo.setAttribute("href", json._embedded.events[i].url)
     card.append(image);
     cardBody.append(cardHeader);
     cardBody.append(dateAndTime);
+    cardBody.append(eventInfo);
     card.append(cardBody);
-    cardDeck.append(card);
+    carouselItem.append(card)
+    cardDeck.append(carouselItem);
     var latitude = parseFloat(json._embedded.events[i]._embedded.venues[0].location.latitude);
     var longitude = parseFloat(json._embedded.events[i]._embedded.venues[0].location.longitude);
     var latlng2 = { lat: latitude, lng: longitude };
@@ -83,14 +92,72 @@ function updateSearch(json) {
     marker.setMap(map);
     map.setCenter(latlng2);
   }
+  $(document).ready(function () {
+    $(".carousel-item:first-child").addClass("active");
+  });
 }
 
 function showEvents(json) {
-  var latlng = { lat: 33.6846, lng: -117.8265 };
+  var markers =[];
+  var infoArray = [];
+  var latlng = { lat: 32.7549, lng: -117.1104 };
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 10,
+    zoom: 8,
     center: latlng
   });
+
+  console.log(json);
+
+  function initMap() {
+    // var latlng = { lat: 33.6846, lng: -117.8265 };
+    // var map = new google.maps.Map(document.getElementById('map'), {
+    //   zoom: 10,
+    //   center: latlng
+    // });
+    // var map = new google.maps.Map(document.getElementById('map'), {
+    //   zoom: 4,
+    //   center: uluru
+    // });
+    var contentString = "<div>" + "<p>" + json._embedded.events[i].name + "</p>" + "</div>"
+
+    var infoWindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+    infoArray.push(infoWindow);
+
+    var latitude = parseFloat(json._embedded.events[i]._embedded.venues[0].location.latitude);
+    var longitude = parseFloat(json._embedded.events[i]._embedded.venues[0].location.longitude);
+    var latLng2 = { lat: latitude, lng: longitude };
+
+    var marker = new google.maps.Marker({
+      position: latLng2,
+      map: map,
+      title: json._embedded.events[i].name
+    });
+
+    markers.push(marker);
+    marker.setMap(map);
+    // infoWindow.open(map, marker);
+    marker.addListener('click', function () {
+
+      // if(marker.setVisible(false)){
+      //   marker.setVisbile(true)
+      // }
+      // infoArray[i].content = "<div>"+"<p>"+ json._embedded.events[i].name+"</p>"+"</div>";
+      // infoArray[i].close();
+      // infoArray[i].open(map, marker)
+      // marker.setVisible(false);
+      // infoWindow.close();
+      infoWindow.open(map, marker);
+      marker.setVisible(false);
+      // if (marker.setVisible(false)) {
+      //   marker.setVisbile(true)
+      // }
+        //  infoWindow.close();
+    });
+    // console.log(markers);
+    // console.log(infoArray);
+  }
 
   function getFormattedTime(hour, minutes) {
     var hours = ((hour + 11) % 12) + 1;
@@ -100,7 +167,10 @@ function showEvents(json) {
   }
 
   for (var i = 0; i < json._embedded.events.length; i++) {
-    var cardDeck = document.querySelector('.scrolling-wrapper');
+    var cardDeck = document.querySelector('.carousel-inner');
+    var carouselItem = document.createElement('div');
+    carouselItem.classList.add("carousel-item");
+    carouselItem.classList.add("carousel-card");
     var card = document.createElement('div');
     card.classList.add("card")
     var image = document.createElement('img');
@@ -110,9 +180,16 @@ function showEvents(json) {
     cardBody.classList.add('card-body', 'd-flex', 'justify-content-center', 'align-items-center', 'flex-column');
     var cardHeader = document.createElement('h5');
     cardHeader.classList.add('card-title');
+    cardHeader.classList.add('text-center');
     cardHeader.textContent = json._embedded.events[i].name;
     var dateAndTime = document.createElement('p');
     dateAndTime.classList.add('card-text');
+
+    var eventInfo = document.createElement('a');
+    eventInfo.classList.add('card-text');
+    eventInfo.textContent = 'Event Info';
+    eventInfo.setAttribute("href", json._embedded.events[i].url)
+
     var eventTime = json._embedded.events[i].dates.start.localTime;
     var eventTime1 = eventTime.slice(0, 2);
     var eventTime2 = eventTime.slice(3, 5);
@@ -124,40 +201,11 @@ function showEvents(json) {
     card.append(image);
     cardBody.append(cardHeader);
     cardBody.append(dateAndTime);
+    cardBody.append(eventInfo);
     card.append(cardBody);
-    cardDeck.append(card);
-    // var latitude = parseFloat(json._embedded.events[i]._embedded.venues[0].location.latitude);
-    // var longitude = parseFloat(json._embedded.events[i]._embedded.venues[0].location.longitude);
-    // var latLng2 = { lat: latitude, lng: longitude };
-    // var latitudeArray = [];
-    // latitudeArray.push(latlng2)
-    for (var i = 0; i < json._embedded.events.length; i++) {
-      var latitude = parseFloat(json._embedded.events[i]._embedded.venues[0].location.latitude);
-      var longitude = parseFloat(json._embedded.events[i]._embedded.venues[0].location.longitude);
-      var latLng2 = { lat: latitude, lng: longitude };
-      var latitudeArray = [];
-      latitudeArray.push(latLng2)
-      // var counter = 0;
-      if(latitudeArray[i] === latLng2) {
-           var currentId = document.getElementById('event'+ toString(i));
-        currentId + " ," + json._embedded.events[i].name;
-      } else {
-        var marker = new google.maps.Marker({
-          position: latLng2,
-          title: json._embedded.events[i].name
-        });
-        marker.setMap(map);
-        marker.id = "event" + toString(i)
-        map.setCenter(latLng2);
-      }
-    }
-    // var marker = new google.maps.Marker({
-    //   position: latLng2,
-    //   title: json._embedded.events[i].name
-    // });
-    // marker.setMap(map);
-    // marker.id = "event" + toString(counter)
-    // map.setCenter(latLng2);
+    carouselItem.append(card)
+    cardDeck.append(carouselItem);
+    initMap();
   }
 
 }
